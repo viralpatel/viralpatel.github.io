14d74b93fb29c0e7076f6c9e13f2c589ce92432c
unknown <viralpatel.net@gmail.com>
2016-09-20 21:21:15 +0530

    PWA

--- pwa/app/js/app.js
+++ pwa/app/js/app.js
@@ -5,32 +5,6 @@ if('serviceWorker' in navigator) {
 }
 
 
-var cacheName = 'infosys-pwa1';  
-var filesToCache = [  
-                    '/pwa/',  
-                    '/pwa/index.html',  
-                    '/pwa/app/api.json',  
-                    '/pwa/app/js/app.js',  
-                    '/pwa/app/css/index.css',  
-                    '/pwa/assets/materialize/css/materialize.min.css',  
-                    '/pwa/assets/jquery-3.1.0/jquery-3.1.0.min.js',  
-                    '/pwa/assets/materialize/js/materialize.min.js'
-                  ];
-
-
-
-self.addEventListener('install', function(e) {  
-  console.log('[ServiceWorker] Install');  
-  e.waitUntil(  
-    caches.open(cacheName).then(function(cache) {  
-      console.log('[ServiceWorker] Caching app shell');  
-      return cache.addAll(filesToCache);  
-    })  
-  );  
-});
-
-
-
 jQuery(function(){
 	
 	
