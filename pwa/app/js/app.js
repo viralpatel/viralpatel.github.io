4bcbcf46ee2d5f4219d4975675644e18610a1896
unknown <viralpatel.net@gmail.com>
2016-09-20 21:41:31 +0530

    PWA

--- pwa/app/js/app.js
+++ pwa/app/js/app.js
@@ -1,7 +1,15 @@
-if('serviceWorker' in navigator) {  
-  navigator.serviceWorker  
-           .register('/pwa/service-worker.js')  
-           .then(function() { console.log('Service Worker Registered'); });  
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('pwa/service-worker.js').then(function(reg) {
+        console.log(':^)', reg);
+        reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).then(function(sub) {
+            console.log('endpoint:', sub.endpoint);
+        });
+    }).catch(function(error) {
+        console.log(':^(', error);
+    });
 }
 
 
