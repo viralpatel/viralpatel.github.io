589965fb1a2ea2691fb6039560ef72d029f88e03
unknown <viralpatel.net@gmail.com>
2016-09-21 12:54:22 +0530

    Remove PWA

--- pwa/app/js/app.js
+++ pwa/app/js/app.js
@@ -1,50 +1,28 @@
-if ('serviceWorker' in navigator) {
-    console.log('Service Worker is supported');
-    navigator.serviceWorker.register('/pwa/service-worker.js').then(function(reg) {
-        console.log(':^)', reg);
-        reg.pushManager.subscribe({
-            userVisibleOnly: true
-        }).then(function(sub) {
-			try{
-            console.log('endpoint:', sub.endpoint);
-			alert(sub.endpoint);
-			$('#content').prepend(sub.endpoint);
-			}catch(e){
-				console.error(e);
-			}
-        });
-    }).catch(function(error) {
-        console.log(':^(', error);
+jQuery(function() {
+
+
+  $('#loader').show();
+
+  jQuery.get('/pwa/app/api.json',
+    function(response) {
+      $('#loader').hide();
+      populateData(response.items);
     });
-}
 
-
-jQuery(function(){
-	
-	
-	$('#loader').show();
-
-	jQuery.get('/pwa/app/api.json', 
-	function(response){	
-		$('#loader').hide();
-		populateData(response.items);
-	});
-	
 });
 
 function populateData(items) {
-	
-	for(var i in items) {
-		
-		var card = $('<div/>').addClass('card').attr('data-link', items[i].link)
-					.append($('<div/>').addClass('card-content')
-						.append(
-							$('<span/>').text(items[i].title).addClass('card-title'),
-							$('<p/>').text(items[i].description)
-						))
-					.appendTo('#items');
-				
-	}
+
+  for (var i in items) {
+
+    var card = $('<div/>').addClass('card').attr('data-link', items[i].link)
+      .append($('<div/>').addClass('card-content')
+        .append(
+          $('<span/>').text(items[i].title).addClass('card-title'),
+          $('<p/>').text(items[i].description)
+        ))
+      .appendTo('#items');
+
+  }
 
 };
-
