a966649b3996340a8ce676e04d9b969ce614f20a
Viral Patel <viralpatel.net@gmail.com>
2016-09-21 15:17:36 +0530

    Added push notifications

---
+++ pwa/service-worker.js
@@ -0,0 +1,20 @@
+
+console.log('Started', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message', event);
+  var title = 'Push message';
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: 'The Message',
+      icon: 'app/images/launcher-icon-4x.png',
+      tag: 'my-tag'
+    }));
+});
+// TODO
