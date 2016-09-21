5d33a9537021fc6622e4df9292e075131d12d122
Viral Patel <viralpatel.net@gmail.com>
2016-09-21 15:23:20 +0530

    Push notification subscription

--- pwa/app/js/app.js
+++ pwa/app/js/app.js
@@ -2,6 +2,17 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker is supported');
     navigator.serviceWorker.register('/pwa/service-worker.js').then(function(reg) {
         console.log(':^)', reg);
+        reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).then(function(sub) {
+			try{
+            console.log('endpoint:', sub.endpoint);
+			//alert(sub.endpoint);
+			//$('#content').prepend(sub.endpoint);
+			}catch(e){
+				console.error(e);
+			}
+        });
     }).catch(function(error) {
         console.log(':^(', error);
     });
